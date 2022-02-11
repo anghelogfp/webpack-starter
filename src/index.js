321510efd548c3c0e5ac10ce81a8d852cc338ded
Anghelo <anghelogfp@gmail.com>
2022-02-10 21:45:39 -0500

    Subuendo App1 TodoList

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
-const nombre = 'Anghelo Gabriel';
+export const todoList = new TodoList();
 
-saludar( nombre );
+todoList.todos.forEach ( todo => crearTodoHtml( todo ) );
+
+console.log(todoList.todos);
